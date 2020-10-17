68b3e2e3d76820f4a69a9372fbc0f21509ec7149
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2020-10-16 20:38:46 -0400

    Updated logs and startblock

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,10 @@
+import {DataSourceTemplate, log } from "@graphprotocol/graph-ts"
 import { newFunder } from '../generated/SecurityToken/SecurityToken'
 import { FundingToken } from '../generated/schema'
 
 export function handleNewFunding(event: newFunder): void {
   let fundingToken = new FundingToken(event.params.funder.toHex())
+  log.info("New funder at address: {}", [event.params.funder.toHex()])
   fundingToken.id = event.params.funder.toHex()
   fundingToken.fundingvalue = event.params.value
   fundingToken.tenor = event.params.tenor
